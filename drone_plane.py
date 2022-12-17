1b5fab51e5e53c06c74335c1ee3de8fa35e9ef00
kouichiUme <kouichi@gmail.com>
2022-12-17 16:16:21 +0900

    copter ichinohe

--- drone_plane.py
+++ drone_plane.py
@@ -26,9 +26,31 @@ try:
     point1 = LocationGlobalRelative(-35.878275,149.165218, 20)
     vehicle.simple_goto(point1)
 
-    #neigber location set 
-    point2 = LocationGlobalRelative(35.867003, 140.305987, 20)
-    vehicle.simple_goto(point2)
+    while(True):
+        # river1 
+        point2 = LocationGlobalRelative(35.87694326855309, 140.33935666245745)
+        # river point 2
+        # 35.87694326855309, 140.33935666245745
+        # 35.86498250692829, 140.32039472643464 
+        vehicle.simple_goto(point2)
+
+        point3 = LocationGlobalRelative(35.86498250692829, 140.32039472643464)
+        vehicle.simple_goto(point3)
+        # 35.86484669941803, 140.30981619440792
+        point4 = LocationGlobalRelative(35.86484669941803, 140.30981619440792)
+        vehicle.simple_goto(point4)
+        # 35.871108231642914, 140.29105598517782
+        # 
+        point5 = LocationGlobalRelative(35.86484669941803, 140.30981619440792)
+        vehicle.simple_goto(point5)
+
+        point4 = LocationGlobalRelative(35.86484669941803, 140.30981619440792)
+        vehicle.simple_goto(point4)
+
+        point3 = LocationGlobalRelative(35.86498250692829, 140.32039472643464)
+        vehicle.simple_goto(point3)
+
+
 
 except TimeoutError as takeoffError:
     print("Takeoff is timeout!!!")
--- main.sh
+++ main.sh
@@ -1,3 +1,5 @@
 python autocopter.py &
 python drone_plane.py &
 
+sim_vehicle.py -v Copter --console --map -D
+sim_vehicle.py -v Copter --console --map -D
