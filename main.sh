6ea9bfc0339066c4f038a6bff5309ad9cc818246
kouichiUme <kouichi@gmail.com>
2022-12-17 16:19:29 +0900

    kidouyou script

--- main.sh
+++ main.sh
@@ -1,5 +1,7 @@
 python autocopter.py &
 python drone_plane.py &
 
-sim_vehicle.py -v Copter --console --map -D
-sim_vehicle.py -v Copter --console --map -D
+sim_vehicle.py -v Copter -L Kawachi -i 1
+sim_vehicle.py -v Copter --map -i 2
+sim_vehicle.py -v Rover -i 3
+sim_vehicle.py -v Rover -L Kawachi --frame motorboat -i 4
