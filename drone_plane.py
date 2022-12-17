c7aaeda63ab989b8ebf39d21a269dfb712ab1325
kouichiUme <kouichi@gmail.com>
2022-12-17 15:53:51 +0900

    port number

--- drone_plane.py
+++ drone_plane.py
@@ -1,9 +1,19 @@
-from dronekit import connect, VehicleMode, Command
+from dronekit import connect, VehicleMode, Command,LocationGlobalRelative
 from pymavlink import mavutil
 import time
 import sys
 
-vehicle = connect('tcp:127.0.0.1:5763',wait_ready=True, timeout=60)
+#
+# tcp:127.0.0.1:5770
+#  copter tcp:127.0.0.1:5763
+#  rover1   tcp:127.0.0.1:5773
+#  rover2   tcp:127.0.0.1:5783
+#  boat1    tcp:127.0.0.1:5793
+#  boat2   tcp:127.0.0.1:5803
+#  boat1    tcp:127.0.0.1:5813
+#  plane   tcp:127.0.0.1:5823
+
+vehicle = connect('tcp:127.0.0.1:5773',wait_ready=True, timeout=60)
 
 #Home location set 
 point1 = LocationGlobalRelative(-35.878275,149.165218, 20)
