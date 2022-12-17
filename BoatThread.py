40909c4e39ab76f64a1ab83927718938ac8da0f3
kouichiUme <kouichi@gmail.com>
2022-12-17 18:25:16 +0900

    main

--- BoatThread.py
+++ BoatThread.py
@@ -1,13 +1,14 @@
 import time
 import threading
-import autocopter 
+import auto_boat
 
-class CopterThread(threading.Thread):
 
+class BoatThread(threading.Thread):
+    
     def __init__(self):
         threading.Thread.__init__(self)
     
     def run(self):
         time.sleep(2)
-        print("main copter")
-        # autocopter.copter()
+        print("main boat")
+
