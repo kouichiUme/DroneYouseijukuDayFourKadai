e1570d39b6eaa9e62b185a827913eeddc67e133b
kouichiUme <kouichi@gmail.com>
2022-12-17 18:36:08 +0900

    delete from

--- main.py
+++
@@ -1,9 +0,0 @@
-from CopterThread import CopterThread
-from BoatThread import BoatThread
-
-def main():
-        copter = CopterThread()
-        copter.start()
-        boat = BoatThread()
-        boat.start()
-main()
